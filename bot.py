6f891f3f6a0e93d3bf518b3fae82a570b0e8b253
PurpleSci <yanikyanus@gmail.com>
2020-10-24 19:07:32 +0300

    Update the bot

--- bot.py
+++ bot.py
@@ -5,28 +5,74 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix="p:")
 
-greetings = ["Hello!", "Hallo!", "Bonjour!", "Ciao!", "¡Hola!",
-             "Hej!", "Ahoj!", "Aloha!", "Привет!", "Χαίρετε!",
-             "你好!", "こんにちは!", "여보세요!", "!مرحبا", "हैलो!"]
+greetings = [
+    "Hello!", "Hallo!", "Bonjour!", "Ciao!", "¡Hola!",
+    "Hej!", "Ahoj!", "Aloha!", "Привет!", "Χαίρετε!",
+    "你好!", "こんにちは!", "여보세요!", "!مرحبا", "हैलो!"
+]
 
-predictions = ["Yeah, sure.", "Probably yes.", "I guess, no.", "What about no?", "Ask again later.", "I'm busy now, you know."]
+predictions = [
+    "Yeah, sure.", "Probably yes.", "Yes, totally.", "Obviously, yes!", 
+    "I guess, no.", "What about no?", "Nah. Not at all.", "Are you kidding me? Of course, no!"
+]
 
-death_scenarios = ["A snake bit you.", "You met a vampire and he sucked all your blood.", \
-                   "You drank a glass of juice, but there was poison inside.", \
-                   "You were thrown out into space by a giant gorilla.", "You ate too much mushrooms.", \
-                   "A heavy hammer fell onto your head.", "A zombie strangled you to death.", \
-                   "A maniac cut your throat while you were sleeping.", "You became too old.", \
-                   "You decided that you've had enough and committed suicide.", \
-                   "You wanted to take a vacation in Chernobyl.", \
-                   "Voldemort came to you and said 'Avada Kedavra!'", \
-                   "You were trying to install Gentoo Linux, but failed."]
+death_scenarios = [
+    "A snake bit you.", "You met a vampire and he sucked all your blood.", \
+    "You drank a glass of juice, but there was poison inside.", \
+    "You were thrown out into space by a giant gorilla.", "You ate too much mushrooms.", \
+    "A heavy hammer fell onto your head.", "A zombie strangled you to death.", \
+    "A maniac cut your throat while you were sleeping.", "You became too old.", \
+    "You decided that you've had enough and committed suicide.", \
+    "You wanted to take a vacation in Chernobyl.", \
+    "Voldemort came to you and said \'Avada Kedavra!\'", \
+    "You were trying to install Arch Linux, but failed."
+]
 
 scary_things = ["😈", "💀", "👻", "🎃", "🧛‍♂️", "🦇", "🧟"]
 
+ubuntu_versions = [
+    "Ubuntu 4.10 (Warty Warthog)", \
+    "Ubuntu 5.04 (Hoary Hedgehog)", \
+	"Ubuntu 5.10 (Breezy Badger)", \
+    "Ubuntu 6.06 LTS (Dapper Drake)", \
+	"Ubuntu 6.10 (Edgy Eft)", \
+	"Ubuntu 7.04 (Feisty Fawn)", \
+	"Ubuntu 7.10 (Gutsy Gibbon)", \
+	"Ubuntu 8.04 LTS (Hardy Heron)", \
+	"Ubuntu 8.10 (Intrepid Ibex)", \
+	"Ubuntu 9.04 (Jaunty Jackalope)", \
+	"Ubuntu 9.10 (Karmic Koala)",  \
+	"Ubuntu 10.04 LTS (Lucid Lynx)",  \
+	"Ubuntu 10.10 (Maverick Meerkat)",  \
+	"Ubuntu 11.04 (Natty Narwhal)",  \
+	"Ubuntu 11.10 (Oneiric Ocelot)",  \
+	"Ubuntu 12.04 LTS (Precise Pangolin)",  \
+	"Ubuntu 12.10 (Quantal Quetzal)",  \
+	"Ubuntu 13.04 (Raring Ringtail)",  \
+	"Ubuntu 13.10 (Saucy Salamander)",  \
+	"Ubuntu 14.04 LTS (Trusty Tahr)",  \
+	"Ubuntu 14.10 (Utopic Unicorn)",  \
+	"Ubuntu 15.04 (Vivid Vervet)",  \
+	"Ubuntu 15.10 (Wily Werewolf)",  \
+	"Ubuntu 16.04 LTS (Xenial Xerus)",  \
+	"Ubuntu 16.10 (Yakkety Yak)",  \
+	"Ubuntu 17.04 (Zesty Zapus)",  \
+	"Ubuntu 17.10 (Artful Aardvark)",  \
+	"Ubuntu 18.04 LTS (Bionic Beaver)",  \
+	"Ubuntu 18.10 (Cosmic Cuttlefish)",  \
+	"Ubuntu 19.04 (Disco Dingo)",  \
+	"Ubuntu 19.10 (Eoan Ermine)",  \
+	"Ubuntu 20.04 LTS (Focal Fossa)",  \
+	"Ubuntu 20.10 (Groovy Gorilla)",  \
+    "Ubuntu 21.04 (Hirsute H...)",
+]
+
 vowels = ["a","e","i","o","u","y"]
 
-consonants = ["b","c","d","f","g","h","j","k","l","m", \
-                 "n","p","q","r","s","t","v","w","x","z"]
+consonants = [
+    "b","c","d","f","g","h","j","k","l","m", \
+    "n","p","q","r","s","t","v","w","x","z"
+]
 
 @client.event
 async def on_connect():
@@ -98,6 +144,10 @@ async def pogchamp(ctx):
 ░░░░░▓▓▒▒░▒░░▓▓░░░░░
 ░░░░░░░▒▒▒▒▒▒▒░░░░░░
 ''')
+
+@client.command()
+async def ubuntu(ctx):
+    await ctx.send(random.choice(ubuntu_versions))
 
 @client.command()
 async def groovy(ctx):
